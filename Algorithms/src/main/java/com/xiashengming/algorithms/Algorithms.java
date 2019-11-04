0745daf42ac0290c96443c2928969497d334932d
xiashengming <xiashengming@baidu.com>
2019-11-04 19:27:37 +0800

    提交～

--- Algorithms/src/main/java/com/xiashengming/algorithms/Algorithms.java
+++ Algorithms/src/main/java/com/xiashengming/algorithms/Algorithms.java
@@ -27,9 +27,57 @@ public class Algorithms {
     }
 
     /**
+     * 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+     *
+     * 说明: 叶子节点是指没有子节点的节点。
+     *
+     * 示例: 
+     * 给定如下二叉树，以及目标和 sum = 22，
+     *
+     *               5
+     *              / \
+     *             4   8
+     *            /   / \
+     *           11  13  4
+     *          /  \      \
+     *         7    2      1
+     * ————————————————
+     * 版权声明：本文为CSDN博主「moots_」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+     * 原文链接：https://blog.csdn.net/shuzishij/article/details/81235076
+     *
+     */
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return root.data == sum;
+        }
+        boolean result = true;
+        result = hasPathSum(root.left, sum - root.data);
+        if (result) {
+            return true;
+        }
+        result = hasPathSum(root.right, sum - root.data);
+        if (result) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
      * 单向链表
      */
     public class SingleNode {
         public SingleNode next;
+    }
+
+    /**
+     * 二叉树
+     */
+    public class TreeNode {
+        public int data;
+        public TreeNode left;
+        public TreeNode right;
     }
 }
